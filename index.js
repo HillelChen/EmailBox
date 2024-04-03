e50cc3b012e9f6876941d9983b2642e2f426ce0b
HillelChen <hillel.chen@gmail.com>
2024-04-03 11:11:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
